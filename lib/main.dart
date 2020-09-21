4c367dfdae07b8068322ed127ac3b6e572dad0a5
Khiem Le <theinnocentman1@gmail.com>
2020-09-21 10:29:44 +0700

    Update 9/2020

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,23 +22,26 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String _message = 'You are not sign in';
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<FirebaseUser> _handleSignIn() async {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  FirebaseAuth _auth;
+  GoogleSignIn _googleSignIn;
+
+  Future<User> _handleSignIn() async {
     final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
     final GoogleSignInAuthentication googleAuth =
         await googleUser.authentication;
 
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
+    final AuthCredential credential = GoogleAuthProvider.credential(
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
 
-    final FirebaseUser user =
-        (await _auth.signInWithCredential(credential)).user;
+    final User user = (await _auth.signInWithCredential(credential)).user;
     print("signed in " + user.displayName);
     setState(() {
+      // User đã login thì hiển thị đã login
       _message = "You are signed in";
     });
     return user;
@@ -46,18 +51,14 @@ class _MyHomePageState extends State<MyHomePage> {
     await _auth.signOut();
     await _googleSignIn.signOut();
     setState(() {
+      // Hiển thị thông báo đã log out
       _message = "You are not sign out";
     });
   }
 
-  @override
-  void initState() {
-    _checkLogin();
-    super.initState();
-  }
-
   Future _checkLogin() async {
-    final FirebaseUser user = await _auth.currentUser();
+    // Khi mở app lên thì check xem user đã login chưa
+    final User user = _auth.currentUser;
     if (user != null) {
       setState(() {
         _message = "You are signed in";
@@ -71,26 +72,41 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('Firebase Login'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(_message),
-            OutlineButton(
-              onPressed: () {
-                _handleSignIn();
-              },
-              child: Text('Login'),
-            ),
-            OutlineButton(
-              onPressed: () {
-                _handleSignOut();
-              },
-              child: Text('Logout'),
-            ),
-          ],
-        ),
-      ),
+      body: FutureBuilder(
+          future: _initialization,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.done) {
+              _auth = FirebaseAuth.instance;
+              _googleSignIn = GoogleSignIn();
+              _checkLogin();
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(_message),
+                    OutlineButton(
+                      onPressed: () {
+                        _handleSignIn();
+                      },
+                      child: Text('Login'),
+                    ),
+                    OutlineButton(
+                      onPressed: () {
+                        _handleSignOut();
+                      },
+                      child: Text('Logout'),
+                    ),
+                  ],
+                ),
+              );
+            }
+
+            return CircularProgressIndicator();
+          }),
     );
   }
 }
